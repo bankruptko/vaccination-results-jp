01ce7a94f6c9bb0aa42a764a0cb5461c04a43e36
bankruptko <bankruptko@protonmail.com>
2021-05-19 17:43:01 +0900

    Fetch vaccination result and convert it to csv

---
+++ postprocess-iryo.ts
@@ -0,0 +1,13 @@
+import { xlsx, readXLSX, writeCSV } from "https://deno.land/x/flat@0.0.10/mod.ts";
+
+const inputFilename = Deno.args[0];
+const outputFilename = inputFilename.replace(".xlsx", ".csv");
+
+// read about what the xlsx library can do here: https://github.com/SheetJS/sheetjs
+
+const workbook = await readXLSX(inputFilename);
+const sheetData = workbook.Sheets[workbook.SheetNames[2]];
+const csvString = await xlsx.utils.sheet_to_csv(sheetData); // can use to_json, to_txt, to_html, to_formulae
+
+// write to csv
+await writeCSV(outputFilename, csvString);
